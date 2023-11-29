1814518218688e5716cb828a3c43f407704003ae
freedom00 <jonathan888999@163.com>
2023-11-29 12:22:41 -0500

    Add project files.

---
+++ IoCPrincipleExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IoCPrincipleExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
